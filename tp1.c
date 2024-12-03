c6d6a0b0f0cf2aa5e57165a313dd1fdbeb16732c
RighiHadjer <righihadjer305@gmail.com>
2024-12-03 07:25:45 +0100

    Update tp1.c

--- tp1.c
+++ tp1.c
@@ -1,1 +1,67 @@
-,LKJMLKMKJM
+#include <stdio.h >
+int main () {
+int matrix [5][5]
+
+for(int i=0;i<5;i++)
+{
+      for(int j =0;j<5;j++)
+    {
+        printf ("%c" ,matrix [i][j]);
+        printf ("/n");
+    }  
+}
+for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        if(i%2=0)
+        {
+              printf ("%c /t"; matrix [i][j]);
+              printf ("/n");
+        }
+   }
+for(i=0;i<5;i++)
+{
+     for(j=0;j<5;j++)
+     if (i%2!=0)
+    {
+         printf ("%c "; matrix [i][j]);
+         printf ("/n");
+    }
+}
+for(i=0;i<5;i++)
+{
+     for(j=0;j<5;j
+    if(i=j)
+    {
+        printf ("%c"; matrix [i][j]);
+        printf ("/n");
+    }
+for(i=0;i<5;i++)
+{
+     for(j=0;j<5;j++)
+     if(i+j=n_1)
+     {
+           printf ("%c /t"; matrix [i][j]);
+           printf ("/n");
+     }
+return 0;
+}
+
+#include <stdio.h >
+int main () {
+int m[10] [10]
+int i ,j , x;
+for(i=0;i<10;i++)
+     for(j=0;j<10;j++)
+           if(i=j)
+       m[i][j]=-20;
+       else  
+       if (i>j)
+       x=m[i][j]
+       m [i][j]=m [j][i]
+       for(i=0;i<10;i++)
+             for (j=0;j<10;j++)
+             printf("%c",matrix[i][j]);
+             printf("\n");
+             return 0;
+}

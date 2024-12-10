6d4c14481920427ab6c523dbcc3632e310e2c777
RighiHadjer <righihadjer305@gmail.com>
2024-12-10 05:32:57 +0100

    Update tp1.c

--- tp1.c
+++ tp1.c
@@ -4,45 +4,45 @@ int matrix [5][5]
 
 for(int i=0;i<5;i++)
 {
-      for(int j =0;j<5;j++)
+   for(int j =0;j<5;j++)
     {
-        printf ("%c" ,matrix [i][j]);
-        printf ("/n");
+       printf ("%c" ,matrix [i][j]);
+    printf ("/n");
     }  
 }
 for(i=0;i<5;i++)
     {
-        for(j=0;j<5;j++)
+      for(j=0;j<5;j++)
         if(i%2=0)
         {
-              printf ("%c /t"; matrix [i][j]);
-              printf ("/n");
-        }
-   }
+            printf ("%c /t"; matrix [i][j]);
+       printf ("/n");
+         }
+     }
 for(i=0;i<5;i++)
 {
-     for(j=0;j<5;j++)
+   for(j=0;j<5;j++)
      if (i%2!=0)
     {
-         printf ("%c "; matrix [i][j]);
-         printf ("/n");
+        printf ("%c "; matrix [i][j]);
+    printf ("/n");
     }
 }
 for(i=0;i<5;i++)
 {
-     for(j=0;j<5;j
-    if(i=j)
+    for(j=0;j<5;j
+      if(i=j)
     {
         printf ("%c"; matrix [i][j]);
-        printf ("/n");
+     printf ("/n");
     }
 for(i=0;i<5;i++)
 {
-     for(j=0;j<5;j++)
+   for(j=0;j<5;j++)
      if(i+j=n_1)
      {
-           printf ("%c /t"; matrix [i][j]);
-           printf ("/n");
+         printf ("%c /t"; matrix [i][j]);
+    printf ("/n");
      }
 return 0;
 }
@@ -53,15 +53,15 @@ int m[10] [10]
 int i ,j , x;
 for(i=0;i<10;i++)
      for(j=0;j<10;j++)
-           if(i=j)
-       m[i][j]=-20;
-       else  
-       if (i>j)
-       x=m[i][j]
-       m [i][j]=m [j][i]
+        if(i=j)
+          m[i][j]=-20;
+        else  
+         if (i>j)
+          x=m[i][j]
+          m [i][j]=m [j][i]
        for(i=0;i<10;i++)
-             for (j=0;j<10;j++)
+          for (j=0;j<10;j++)
              printf("%c",matrix[i][j]);
-             printf("\n");
+           printf("\n");
              return 0;
 }

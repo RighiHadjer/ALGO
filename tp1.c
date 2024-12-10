9e81419db8d6b8de30aec85d9911087741ee88b5
RighiHadjer <righihadjer305@gmail.com>
2024-12-10 09:23:49 +0100

    Update tp1.c

--- tp1.c
+++ tp1.c
@@ -1,76 +1,89 @@
-#include <stdio.h >
-int main () {
-int matrix [5][5];
-int i,j,n;
-for(int i=0;i<5;i++)
-{
-   for(int j =0;j<5;j++)
-    {
-       printf ("%c" ,matrix [i][j]);
-    printf ("/n");
-    }  
-}
-for(i=0;i<5;i++)
-    {
-      for(j=0;j<5;j++)
-        if(i%2=0)
-        {
-            printf ("%c /t"; matrix [i][j]);
-       printf ("/n");
-         }
-     }
-for(i=0;i<5;i++)
-{
-   for(j=0;j<5;j++)
-     if (i%2!=0)
-    {
-        printf ("%c "; matrix [i][j]);
-    printf ("/n");
+#include<stdio.h>
+
+int main() {
+    int matrix[5][5];
+    int value = 1;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            matrix[i][j] = value++;
+        }
     }
-}
-for(i=0;i<5;i++)
-{
-    for(j=0;j<5;j
-      if(i=j)
-    {
-        printf ("%c"; matrix [i][j]);
-     printf ("/n");
+    printf("La matrice complète:\n");
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
     }
-for(i=0;i<5;i++)
-{
-   for(j=0;j<5;j++)
-     if(i+j=n_1)
-     {
-        printf ("%c /t"; matrix [i][j]);
-    printf ("/n");
-     }
-return 0;
+    printf("\nLes valeurs des lignes paires:\n");
+    for (int i = 0; i < 5; i++) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < 5; j++) {
+                printf("%d\t", matrix[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    printf("\nLes valeurs des lignes impaires:\n");
+    for (int i = 0; i < 5; i++) {
+        if (i % 2 != 0) {
+            for (int j = 0; j < 5; j++) {
+                printf("%d\t", matrix[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    printf("\nLes valeurs sur la diagonale principale:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d\t", matrix[i][i]);
+    }
+    printf("\n");
+    printf("\nLes valeurs sur la diagonale secondaire:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d\t", matrix[i][4 - i]);
+    }
+    printf("\n");
+
+    return 0;
 }
 
-#include <stdio.h >
-int main () {
-int m[10] [10];
-int i ,j , x;
-for(i=0;i<10;i++) 
-{
-     for(j=0;j<10;j++)
-     {
-        if(i=j)
-          m[i][j]=-20;
-        else  
-         {    
-          x=m[i][j];
-          m [i][j]=m [j][i];
-          }
-      }
- }
-       for(i=0;i<10;i++)
-       {
-          for (j=0;j<10;j++)
-           {
-             printf("%c",matrix[i][j]);
-           printf("\n");
-            }
-        }
-             return 0;
+
+
+#include <stdio.h>
+
+int main() {
+    int T[10][10];
+    int i, j, size, temp;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            printf("Enter T[%d][%d]: ", i, j);
+            scanf("%d", &T[i][j]);
+        }
+    }
+    printf("\nOriginal array:\n");
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            printf("%d\t", T[i][j]);
+        }
+        printf("\n");
+    }
+    for (i = 1; i < size; i++) {
+        for (j = 0; j < i; j++) {
+            temp = T[i][j];
+            T[i][j] = T[j][i];
+            T[j][i] = temp;
+        }
+    }
+    printf("\nReversed array:\n");
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            printf("%d\t", T[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }

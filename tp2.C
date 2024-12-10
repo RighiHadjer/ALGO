34dfad04168d99bba205c8317e68ebbc2fa8c65c
RighiHadjer <righihadjer305@gmail.com>
2024-12-10 08:37:20 +0100

    Update tp2.C

--- tp2.C
+++ tp2.C
@@ -1,73 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *ChargerChaine(int N) {
-    char *chaine = (char *)malloc(N + 1); 
-    if (chaine == NULL) {
-        printf("خطأ في تخصيص الذاكرة\n");
-        exit(1);
-    }
-
-    int i = 0;
-    char c;
-    while ((c = getchar()) != '\n' && i < N) {
-        chaine[i++] = c;
-    }
-    chaine[i] = '\0'; 
-    return chaine;
-}
-
-int longueur(char *chaine) {
-    int i = 0;
-    while (chaine[i] != '\0') {
-        i++;
-    }
-    return i;
-}
-
-void copier(char *source, char *destination) {
-    int i = 0;
-    while (source[i] != '\0') {
-        destination[i] = source[i];
-        i++;
-    }
-    destination[i] = '\0';
-}
-
-void inverser(char *chaine) {
-    int i = 0, j = longueur(chaine) - 1;
-    char temp;
-    while (i < j) {
-        temp = chaine[i];
-        chaine[i] = chaine[j];
-        chaine[j] = temp;
-        i++;
-        j--;
-    }
-}
-
-void afficher(char *chaine) {
-    printf("%s\n", chaine);
-}
+// Prototypes de fonctions
+char *ChargerChaine(int N);
+int LongueurChaine(char *str);
+void InverserTableau(char Tableau[], char Inverse[], int longueur);
+void ChargerTableau(char *str, char Tableau[]);
+void AfficherTableau(char Tableau[], int longueur);
 
 int main() {
-    int n;
-    printf("donnélongueur: ");
-    scanf("%d", &n);
+    char *str;
+    int n;
+    printf("Veuillez saisir la taille maximale de la chaîne : ");
+    scanf("%d", &n);
+    str = ChargerChaine(n);
+    int longueur = LongueurChaine(str);
+    char Tableau[longueur], Inverse[longueur];
+    ChargerTableau(str, Tableau);
+    AfficherTableau(Tableau, longueur);
+    InverserTableau(Tableau, Inverse, longueur);
+    AfficherTableau(Inverse, longueur);
+    free(str);
+    return 0;
+}
 
-    char *chaine = ChargerChaine(n);
+// Définitions des fonctions
 
-    int longueurChaine = longueur(chaine);
-    printf("longueurchaine: %d\n", longueurChaine);
+// Fonction pour charger une chaîne de taille maximale N
+char *ChargerChaine(int N) {
+    char *str = (char *)malloc(N * sizeof(char));
+    printf("Veuillez saisir la chaîne : ");
+    scanf("%s", str);
+    return str;
+}
 
-    char chaineCopie[longueurChaine + 1];
-    copier(chaine, chaineCopie);
-    printf("chainecopie: %s\n", chaineCopie);
+// Fonction pour obtenir la longueur d'une chaîne
+int LongueurChaine(char *str) {
+    int longueur_chaine = 0;
+    while (str[longueur_chaine] != '\0') {
+        longueur_chaine++;
+    }
+    return longueur_chaine;
+}
 
-    inverser(chaine);
-    printf("chaineinversée: %s\n", chaine);
+// Fonction pour inverser le tableau
+void InverserTableau(char Tableau[], char Inverse[], int longueur) {
+    for (int i = 0; i < longueur; i++) {
+        Inverse[i] = Tableau[longueur - 1 - i];
+    }
+}
 
-    free(chaine);
+// Fonction pour charger le tableau à partir de la chaîne
+void ChargerTableau(char *str, char Tableau[]) {
+    int i = 0;
+    while (str[i] != '\0') {
+        Tableau[i] = str[i];
+        i++;
+    }
+    Tableau[i] = '\0';
+}
 
-    return 0;
+// Fonction pour afficher le tableau
+void AfficherTableau(char Tableau[], int longueur) {
+    for (int i = 0; i < longueur; i++) {
+        printf("%c", Tableau[i]);
+    }
+    printf("\n"); 
+return 0;
 }
